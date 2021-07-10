1d1ff16e5884d15ec78a228b611248aa296d7fbd
zhujiqian <924931408@qq.com>
2021-07-10 21:30:32 +0800

    增加用户映射角色逻辑

--- fte-admin/fte-server/src/main/java/com/zhu/fte/biz/service/impl/SysUserRoleServiceImpl.java
+++ fte-admin/fte-server/src/main/java/com/zhu/fte/biz/service/impl/SysUserRoleServiceImpl.java
@@ -1,10 +1,14 @@
 package com.zhu.fte.biz.service.impl;
 
+import com.zhu.fte.biz.common.http.RestResponse;
 import com.zhu.fte.biz.entity.SysUserRole;
 import com.zhu.fte.biz.mapper.SysUserRoleMapper;
 import com.zhu.fte.biz.service.SysUserRoleService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class SysUserRoleServiceImpl extends ServiceImpl<SysUserRoleMapper, SysUserRole> implements SysUserRoleService {
 
+    @Resource
+    SysUserRoleMapper sysUserRoleMapper;
+
+    @Override
+    public RestResponse add(SysUserRole sysUserRole) {
+        return RestResponse.ok( sysUserRoleMapper.insert(sysUserRole));
+    }
+
+    @Override
+    public RestResponse updata(SysUserRole sysUserRole) {
+        return RestResponse.ok(sysUserRoleMapper.updateById(sysUserRole));
+    }
+
+    @Override
+    public RestResponse delete(String id) {
+        return RestResponse.ok(sysUserRoleMapper.deleteById(id));
+    }
+
+    @Override
+    public RestResponse<List<SysUserRole>> getList() {
+        return RestResponse.ok(sysUserRoleMapper.selectList(null));
+    }
 }
